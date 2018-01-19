f612671026f6be87b9d8ad7959f583be1ed11db2
952928837@qq.com <19960303m>
2018-01-19 13:55:00 +0800

    create provider-user

---
+++ spring-cloud-provider-user/src/main/java/com/company/project/service/UserinfoService.java
@@ -0,0 +1,11 @@
+package com.company.project.service;
+import com.company.project.model.Userinfo;
+import com.company.project.core.Service;
+
+
+/**
+ * Created by CodeGenerator on 2018/01/18.
+ */
+public interface UserinfoService extends Service<Userinfo> {
+
+}
