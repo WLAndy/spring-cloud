ea63f2f575b950982b7dcd27c356a1f03024712f
952928837@qq.com <19960303m>
2018-01-19 13:58:58 +0800

    create consumer-movie

---
+++ spring-cloud-consumer-movie/src/main/java/com/company/project/web/ConsumerController.java
@@ -0,0 +1,9 @@
+package com.company.project.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ResponseBody
+@RequestMapping("/consumer")
+public class ConsumerController {
+}
